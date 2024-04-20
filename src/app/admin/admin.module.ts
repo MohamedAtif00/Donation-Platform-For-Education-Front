dc1af653db82777e080b9b1df5746ce4024dac35
MohamedAtif00 <mohamed141462013@gmail.com>
2024-04-20 17:26:42 +0200

    removing-unused

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import { StudentListComponent } from './main/student-list/student-list.component';
-import { DonorListComponent } from './main/donor-list/donor-list.component';
-import { MaterialListComponent } from './main/material-list/material-list.component';
 import { RouterModule } from '@angular/router';
 import { AdminRoutingModule } from './admin-routing/admin-routing.module';
 import { RequestListComponent } from './main/request-list/request-list.component';
@@ -15,9 +12,6 @@ import { RequestListComponent } from './main/request-list/request-list.component
   declarations: [
     HeaderComponent,
     MainComponent,
-    StudentListComponent,
-    DonorListComponent,
-    MaterialListComponent,
     RequestListComponent
   ],
   imports: [
